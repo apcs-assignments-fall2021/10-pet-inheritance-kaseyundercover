e92e95cd59c2f6207f4eccd9ac3032f17fb0b5f9
kaseyk9 <23kkazliner@athenian.org>
2021-10-31 11:41:17 -0700

    Done

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,43 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String name;
+    private int age;
+    private String breed;
 
     // Constructors
-    
+    public Dog(String a, int b, String c) {
+        super(a, b);
+        this.breed = c;
+    }
+    public Dog() {
+        this.name = "Fido";
+        this.age = 1;
+    }
+
 
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Bark!");
+    }
 
     // toString method
+    public String toString() {
+        String str = "Name: " + this.getName() + ", Age: " + this.getAge() + ", Breed: " + this.breed;
+        return str;
+    }
 
 
     // Getter
+    public String getBreed() {
+        return this.breed;
+    }
 
 
     // Setter
+    public void setBreed(String breed) {
+        // Don't allow blank breed
+        if (breed.trim().length() != 0) {
+            this.breed = breed;
+        }
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,7 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog p1 = new Dog("Alfie", 2, "Chihuahua");
+        System.out.println(p1);
+
     }
 }
